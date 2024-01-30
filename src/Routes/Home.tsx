2ccb3c514e1d5826d3902ae0eeeb3ec7dbd6927c
sellen <austin0801@naver.com>
2024-01-30 17:43:56 +0900

    Header part - 2

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 function Home() {
     return (
-        <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>
+        <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>
+            <h1>Home</h1>
+        </div>
     );
 }
 export default Home;
