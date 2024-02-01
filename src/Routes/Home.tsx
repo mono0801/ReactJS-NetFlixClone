e7f11df60532c4ee1f63cb8c066219497d238c4d
sellen <austin0801@naver.com>
2024-02-01 16:26:13 +0900

    Home Slider - 1

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,6 @@
 // 메인 화면
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
@@ -6,6 +8,7 @@ import { makeImagePath } from "../utils";
 
 const Wrapper = styled.div`
     background: black;
+    overflow-x: hidden;
 `;
 // API 로딩 중일 때 표시
 const Loader = styled.div`
@@ -36,6 +39,35 @@ const OverView = styled.p`
     width: 50%;
     font-size: 20px;
 `;
+// 배너 하단에 들어갈 영화 포스터 슬라이드
+const Slider = styled.div`
+    position: relative;
+    top: -100px;
+`;
+// 슬라이드에 들어갈 div
+const Row = styled(motion.div)`
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 10px;
+    position: absolute;
+`;
+// Row안에 들어갈 영화 포스터 div
+const Box = styled(motion.div)`
+    background-color: white;
+    height: 200px;
+    color: red;
+    font-size: 200%;
+`;
+// Row 슬라이드 애니메이션 설정
+const rowVariants = {
+    hidden: {
+        x: window.outerWidth + 10,
+        opacity: 0,
+    },
+    visible: { x: 0, opacity: 1 },
+    exit: { x: -window.outerWidth - 10, opacity: 0 },
+};
 
 function Home() {
     // themoviedb.org로 부터 가져온 영화 정보
@@ -43,7 +75,10 @@ function Home() {
         ["movies", "nowPlaying"],
         getMovies
     );
-    console.log(data, isLoading);
+    // slider에서 현재 Box가 무엇인지 식별
+    const [index, setIndex] = useState(0);
+    // index 증가
+    const increaseIndex = () => setIndex((prev) => prev + 1);
 
     // <></> : fragment -> 많은 요소를 공통된 부모 없이 연이어서 리턴할 때 사용
     return (
@@ -53,6 +88,7 @@ function Home() {
             ) : (
                 <>
                     <Banner
+                        onClick={increaseIndex}
                         // 영화 포스터 전달
                         bgPhoto={makeImagePath(
                             data?.results[0].backdrop_path || ""
@@ -62,6 +98,23 @@ function Home() {
                         <Title>{data?.results[0].title}</Title>
                         <OverView>{data?.results[0].overview}</OverView>
                     </Banner>
+                    <Slider>
+                        {/* 요소가 생기거나 사라질 때 효과 부여 */}
+                        <AnimatePresence>
+                            <Row
+                                key={index}
+                                variants={rowVariants}
+                                initial="hidden"
+                                animate="visible"
+                                exit="exit"
+                                transition={{ type: "tween", duration: 0.5 }}
+                            >
+                                {[1, 2, 3, 4, 5, 6].map((i) => (
+                                    <Box key={i}>{i}</Box>
+                                ))}
+                            </Row>
+                        </AnimatePresence>
+                    </Slider>
                 </>
             )}
         </Wrapper>
